e95cd625c10a06cdac8d7e20cc03f381e011c6a2
thefirstkhocee <hilaryoras@gmail.com>
2024-06-13 06:46:24 +0000

    avalanche-hypersdk-tokenvm

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "hsubber"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "hsub"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "hsub"
 )
 
 var ID ids.ID
